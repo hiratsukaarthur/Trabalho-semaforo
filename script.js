5ed06d27c67d68ccc6d2db96ddc25f66702020fa
hiratsukaarthur <hiratsuka.arthur@gmail.com>
2026-05-26 16:16:54 -0300

    script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+let vermelho = document.getElementById("vermelho");
+let amarelo = document.getElementById("amarelo");
+let verde = document.getElementById("verde");
+
+setTimeout(() => {
+    verde.classList.remove("ativo");
+    amarelo.classList.add("ativo");
+},10000);
+
+setTimeout(() => {
+    amarelo.classList.remove("ativo");
+    vermelho.classList.add("ativo");
+},15000);
